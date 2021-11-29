ba735d2045906041021a041e0cb5b4d4d30e99d1
zhangqiming897 <646452839@qq.com>
2021-11-29 15:26:43 +0800

    代码修改上传

--- src/carousel/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react';
-interface propsInfoType {
-    imgArr?: Array<string>;
-    height?: number;
-    transitionTime?: number;
-}
-declare const Carousel: (props: propsInfoType) => ReactElement;
-export default Carousel;
